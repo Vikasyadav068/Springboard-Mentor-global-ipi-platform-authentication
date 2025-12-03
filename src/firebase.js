724ba9d77dc83e3bd8698405e8f978bf6aaf4b68
Vikas <vikaskumaryadav068@gmail.com>
2025-12-03 15:52:43 +0530

    refactor: Move Firebase config to environment variables
    - Create .env file with Firebase configuration variables
    - Update firebase.js to use process.env variables
    - Add .env.example file as template for other developers
    - Update .gitignore to exclude .env file from version control
    - Improve security by keeping sensitive config out of source code

--- src/firebase.js
+++ src/firebase.js
@@ -6,12 +6,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBASp-eDHaNIMWQIj-ao3DXOMgyT1c4gKs",
-  authDomain: "react-auth-app-710f1.firebaseapp.com",
-  projectId: "react-auth-app-710f1",
-  storageBucket: "react-auth-app-710f1.firebasestorage.app",
-  messagingSenderId: "420066187070",
-  appId: "1:420066187070:web:c1b7ef4f0cb6123984fe6f"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
